bfb190e38a7838195f124a07cad2f4570484dbdf
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-11-25 22:50:20 +0530

    feat: add vercel.json for add multipage routing suport on edge servers

---
+++ frontend/.vercel/output/static/assets/index-CfwRgLOa.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CjKGLORb.js";function n(){return o.jsx("div",{children:'Hello "/dashboard/quotations/"!'})}export{n as component};
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,26 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import vercel from "vite-plugin-vercel";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     devtools(),
     tanstackRouter({
-      target: 'react',
+      target: "react",
       autoCodeSplitting: true,
     }),
     viteReact(),
     tailwindcss(),
-    
+    vercel(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
