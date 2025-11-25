4c76d66fc1f0f3165d775726ac829e55fd1e822f
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-11-25 23:25:47 +0530

    fix: fix vercel.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "vite-plugin-vercel";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { fileURLToPath, URL } from "node:url";
 
@@ -16,7 +15,6 @@ export default defineConfig({
     }),
     viteReact(),
     tailwindcss(),
-    vercel(),
   ],
   resolve: {
     alias: {
