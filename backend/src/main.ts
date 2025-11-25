c3dae3926805f59936696a2716b1a7b5397a285f
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-11-25 21:10:51 +0530

    fix: remove Scalar API Reference

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { apiReference } from '@scalar/nestjs-api-reference';
 import path from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { Response } from 'express';
+
 async function bootstrap() {
   const logger = new Logger();
 
@@ -22,9 +22,10 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
   app.useStaticAssets(path.join(__dirname, '..', 'public'));
+
   const config = new DocumentBuilder()
-    .setTitle('Quotation-Management apis documentation')
-    .setDescription('This is the documentation for Quotation-Management api.')
+    .setTitle('Quotation-Management API Documentation')
+    .setDescription('This is the documentation for Quotation-Management API.')
     .setVersion('1.0')
     .addTag('Notes')
     .build();
@@ -35,20 +36,11 @@ async function bootstrap() {
     res.json(document);
   });
 
-  app.use(
-    '/docs',
-    apiReference({
-      content: document,
-      layout: 'modern',
-      metaData: {
-        title: 'Quotation-Management API Reference',
-        description: 'Beautiful API docs using Scalar',
-      },
-      searchHotKey: 'h',
-      theme: 'bluePlanet',
-      // showToolbar: 'never',
-    }),
-  );
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   await app.listen(process.env.PORT as string);
 
@@ -62,4 +54,5 @@ async function bootstrap() {
     `OpenAPI JSON at http://${process.env.LOCAL_IP}:${process.env.PORT}/openapi.json`,
   );
 }
+
 bootstrap();
